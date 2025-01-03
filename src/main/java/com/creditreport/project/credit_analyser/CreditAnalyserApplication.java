2158301979699cd9d3fe9d04ac0d41d2b9ed7725
Jerin-T <c0896235@mylambton.ca>
2025-01-03 10:00:00 -0500

    Initial commit

---
+++ src/main/java/com/creditreport/project/credit_analyser/CreditAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.creditreport.project.credit_analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAnalyserApplication.class, args);
+	}
+
+}
